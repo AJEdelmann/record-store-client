8629f1078f90606d17e959776d92d803f9dec022
ajedelmann <albert.edelmann@gmail.com>
2020-03-11 13:43:02 +0100

    Setting up thunk for login workflow

---
+++ src/reducers/mainReducer.js
@@ -0,0 +1,37 @@
+const initialState = {
+  isLoggedIn: false,
+  info: {},
+  records: [],
+  loading: true
+};
+
+const mainReducer = (state = initialState, action) => {
+  if (action.type === "HANDLE_LOGIN") {
+    if (action.payload.hasOwnProperty("error")) {
+      state.isLoggedIn = false;
+    } else {
+      state.isLoggedIn = true;
+      state.info = action.payload;
+    }
+
+    state.loading = false;
+
+    return Object.assign({}, state);
+  }
+
+  if (action.type === "HANDLE_LOGOUT") {
+    state.isLoggedIn = false;
+    return Object.assign({}, state);
+  }
+
+  if (action.type === "FETCH_RECORDS") {
+    state.isLoggedIn = true;
+    state.records = [...state.records, ...action.payload];
+
+    return Object.assign({}, state);
+  }
+
+  return state;
+};
+
+export default mainReducer;
